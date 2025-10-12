ae6c5f341c66b38a11cdb24faa854ffbcb172c3d
Alex Fitterling <info@sp33c.tech>
2025-10-12 20:40:53 +0200

    catch e

--- tools/newUser.js
+++ tools/newUser.js
@@ -8,19 +8,23 @@ const {
 const client = new CognitoIdentityProviderClient({});
 
 async function createUser() {
-    const command = new AdminCreateUserCommand({
-        UserPoolId: process.env.USER_POOL_ID,
-        Username: process.env.USER_EMAIL,
-        TemporaryPassword: process.env.PASSWORD, // must meet Cognito password policy
-        MessageAction: "SUPPRESS", // don't send invitation email
-        UserAttributes: [
-            { Name: "email", Value: process.env.USER_EMAIL },
-            { Name: "email_verified", Value: "true" },
-        ],
-    });
+    try {
+        const command = new AdminCreateUserCommand({
+            UserPoolId: process.env.USER_POOL_ID,
+            Username: process.env.USER_EMAIL,
+            TemporaryPassword: process.env.PASSWORD, // must meet Cognito password policy
+            MessageAction: "SUPPRESS", // don't send invitation email
+            UserAttributes: [
+                { Name: "email", Value: process.env.USER_EMAIL },
+                { Name: "email_verified", Value: "true" },
+            ],
+        });
 
-    await client.send(command);
-    console.log("✅ User created");
+        await client.send(command);
+        console.log("✅ User created");
+    } catch (e) {
+        console.log("error occured", e.message);
+    }
 }
 
 createUser().catch(console.error);
